d3c141c21895911f581873b1466dd36d48ccc21b
Shaun Williams <shaunewilliams@gmail.com>
2013-12-22 11:32:47 -0600

    moved youtube iframe code to html page

--- src/main.js
+++ src/main.js
@@ -7,22 +7,6 @@ window.addEventListener('load', function() {
 	var h = Blind.canvas.height = w/16*9;
 	Blind.input.setBorderSize(20);
 
-	(function loopYoutubeSong(){
-		// Code taken from PuzzleScript:
-		// (https://github.com/increpare/PuzzleScript/blob/master/js/engine.js#L650)
-		var youtubeid = 'cRzZ0AdFHBs';
-		var url = "https://youtube.googleapis.com/v/"+youtubeid+"?autoplay=1&loop=1&playlist="+youtubeid;
-		ifrm = document.createElement("IFRAME");
-		ifrm.setAttribute("src",url);
-		ifrm.style.visibility="hidden";
-		ifrm.style.width="500px";
-		ifrm.style.height="500px";
-		ifrm.style.position="absolute";
-		ifrm.style.top="-1000px";
-		ifrm.style.left="-1000px";
-		document.body.appendChild(ifrm);
-	})();
-
 	Blind.assets.load(function(){
 		Blind.input.init();
 		Blind.setScene(Blind.scene_blare);
